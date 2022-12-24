d10cb47cd56006f2f545f911c2947fdd7cd5385d
Eben@ysh <77492217+EbenGitHub@users.noreply.github.com>
2022-12-24 13:50:17 +0300

    pascal binomial trg up chg

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -11,12 +11,12 @@ def pascal_triangle(n):
             n (int): number of lists and digits
         Returns: list of lists
     """
-    start_triangle = [[1] * i for i in range(1, n + 1)]
+    final_triangle = [[1] * i for i in range(1, n + 1)]
     i = 1
     while i < n:
-        for j in range(1, len(start_triangle[i - 1])):
-            start_triangle[i][j] = \
-            start_triangle[i - 1][j] + \
-            start_triangle[i - 1][j - 1]
+        for j in range(1, len(final_triangle[i - 1])):
+            final_triangle[i][j] = \
+            final_triangle[i - 1][j] + \
+            final_triangle[i - 1][j - 1]
         i += 1
-    return start_triangle
+    return final_triangle
