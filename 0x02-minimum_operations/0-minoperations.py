0fdf4f13ebb8931aec59690172529efae373ca86
EbenGitHub <abenezergoo@gmail.com>
2023-01-10 00:51:17 +0300

    copy past op cntr

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+    0-minoperations.py: cminOperations(n)
+"""
+
+
+def copy_all(h, c, o):
+    """
+        copies the content of h to c
+        Args:
+            h: 'H'
+            c: current counter
+            0: operation counter
+        Returns: tuple of current counter and operation counter
+    """
+    c = len(h)
+    o += 1
+    return (c, o)
+
+
+def paste_(h, c, o):
+    """
+        pastes the content of c to h
+        Args:
+            h: 'H'
+            c: current counter
+            0: operation counter
+        Returns: tuple of h and operation counter
+    """
+    h = 'H' * (len(h) + c)
+    o += 1
+    return (h, o)
+
+
+def minOperations(n):
+    """
+        counts the operation done to perfom the given task
+        Args:
+            n: number of times for 'H'
+        Returns: the number of operations done
+    """
+    h = 'H'
+    current = len(h)
+    operation = 0
+    while True:
+        if n == len(h):
+            return operation
+        elif n < len(h):
+            return 0
+        if n < 2 * len(h):
+            (h, operation) = paste_(h, current, operation)
+        else:
+            (current, operation) = copy_all(h, current, operation)
+            (h, operation) = paste_(h, current, operation)
