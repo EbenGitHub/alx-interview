941dd678ff767a3fcb7f896267b68719fbd9ebb5
EbenGitHub <abenezergoo@gmail.com>
2023-01-19 14:18:24 +0300

    try to fix time prob

---
+++ 0x02-minimum_operations/main_1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+Main file for copy/paste algorithm
+"""
+
+minOperations = __import__('0-minoperations').minOperations
+
+print(minOperations(19170307))
---
+++ 0x02-minimum_operations/main_6.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Main file for copy/paste algorithm
+"""
+import sys
+
+minOperations = __import__('0-minoperations').minOperations
+
+print(minOperations(2147483640))
