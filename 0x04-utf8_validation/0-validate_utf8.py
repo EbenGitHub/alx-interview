7ff3692acfef71fe89f59774b11adfee7e4d071a
EbenGitHub <abenezergoo@gmail.com>
2023-01-26 11:03:16 +0300

    lint corr

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -10,9 +10,11 @@
 def validUTF8(data):
     """
         A character in UTF8 can be from 1 to 4 bytes long.
-        For a 1-byte character, the first bit is a 0,
+
+        - For a 1-byte character, the first bit is a 0,
         followed by its Unicode code.
-        For an n-bytes character,
+
+        - For an n-bytes character,
         the first n bits are all one's, the n + 1 bit is 0,
         followed by n - 1 bytes with the most
         significant 2 bits being 10.
