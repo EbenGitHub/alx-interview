9cca5fe915c8bd60316ca5676d6a17149ca7f77e
EbenGitHub <abenezergoo@gmail.com>
2023-01-26 09:31:03 +0300

    pycodestyle lint corr

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
     UTF-8-validation:
-    Given an integer array data representing the data, 
-    return whether it is a valid UTF-8 encoding 
+    Given an integer array data representing the data,
+    return whether it is a valid UTF-8 encoding
     (i.e. it translates to a sequence of valid UTF-8 encoded characters).
 """
 
@@ -10,23 +10,26 @@
 def validUTF8(data):
     """
         A character in UTF8 can be from 1 to 4 bytes long.
-        For a 1-byte character, the first bit is a 0, followed by its Unicode code.
-        For an n-bytes character, the first n bits are all one's, the n + 1 bit is 0, 
-        followed by n - 1 bytes with the most significant 2 bits being 10.
+        For a 1-byte character, the first bit is a 0,
+        followed by its Unicode code.
+        For an n-bytes character,
+        the first n bits are all one's, the n + 1 bit is 0,
+        followed by n - 1 bytes with the most
+        significant 2 bits being 10.
 
         This is how the UTF-8 encoding would work:
 
-        _______________________________________________________________________________________
-        |                    Number of Bytes   |        UTF-8 Octet Sequence                  |
-        |                                      |              (binary)                        |
-        |               --------------------+-----------------------------------------        |
-        |                           1          |   0xxxxxxx                                   |
-        |                           2          |   110xxxxx 10xxxxxx                          |
-        |                           3          |   1110xxxx 10xxxxxx 10xxxxxx                 |
-        |                           4          |   11110xxx 10xxxxxx 10xxxxxx 10xxxxxx        |
-        |_____________________________________________________________________________________|
+        _____________________________________________________________________
+        |  Number of Bytes   |        UTF-8 Octet Sequence                  |
+        |                    |              (binary)                        |
+        |--------------------+-----------------------------------------     |
+        |         1          |   0xxxxxxx                                   |
+        |         2          |   110xxxxx 10xxxxxx                          |
+        |         3          |   1110xxxx 10xxxxxx 10xxxxxx                 |
+        |         4          |   11110xxx 10xxxxxx 10xxxxxx 10xxxxxx        |
+        |___________________________________________________________________|
 
-                        x denotes a bit in the binary form of a byte that may be either 0 or 1.
+        x denotes a bit in the binary form of a byte that may be either 0 or 1.
     """
 
     def check(num):
@@ -46,6 +49,7 @@ def validUTF8(data):
             return False
         while i < len(data) and i <= k:
             cur = check(data[i])
-            if cur != 1: return False
+            if cur != 1:
+                return False
             i += 1
     return True
