f1efea66fcf4d00a8a6b42b7aa2a3bff64fe8e11
Ebenezer Eshetie <77492217+EbenGitHub@users.noreply.github.com>
2023-02-10 02:11:11 +0300

    semistand chck

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -7,7 +7,7 @@ const filmId = process.argv[2];
 request(`${endpoint}/films/${filmId}/`, async function (error, response, body) {
   if (error) return console.log(error);
 
-  let characters = JSON.parse(body).characters;
+  const characters = JSON.parse(body).characters;
 
   for (const character of characters) {
     await new Promise((resolve, reject) => {
